0104e6bf2faf7e8a819a76a317caaa7d258cc05b
Dlcrz <n00215136@upn.pe>
2025-06-04 12:28:56 -0500

    v1

---
+++ src/webparts/descargarDocumento/components/IDescargarDocumentoProps.ts
@@ -0,0 +1,7 @@
+export interface IDescargarDocumentoProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/descargarDocumento/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IDescargarDocumentoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'DescargarDocumentoWebPartStrings' {
+  const strings: IDescargarDocumentoWebPartStrings;
+  export = strings;
+}
